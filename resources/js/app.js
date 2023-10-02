6b55f9793c447a54637268f76f663ac1848d7fc4
Aung-Thu-Zaw <aungthuzaw223@gmail.com>
2023-10-02 13:09:17 +0630

    project init with vue and laravel

---
+++ resources/js/app.js
@@ -0,0 +1,6 @@
+import "./bootstrap";
+
+import { createApp } from "vue";
+import App from "../js/components/App.vue";
+
+createApp(App).mount("#app");
---
+++ resources/views/home.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+    @vite('resources/css/app.css')
+
+
+
+    <style>
+        .container {
+            padding: 20px;
+        }
+
+        .title {
+            color: rgba(0, 0, 0, 0.822);
+            width: 100%;
+            text-align: center;
+            font-family: sans-serif;
+            font-size: 1.5rem;
+        }
+    </style>
+</head>
+
+<body class="antialiased">
+    <div class="container">
+        <h1 class="title">Filepond Image Upload with Laravel + Vue Js</h1>
+
+        <div id="app">
+
+        </div>
+    </div>
+
+    @vite("resources/js/app.js")
+</body>
+
+</html>
